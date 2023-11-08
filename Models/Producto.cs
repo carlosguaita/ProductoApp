f17494bbbd4ac789417ccb2db8d40f73994526c3
carlos.guaita <carlos.guaita@udla.edu.ec>
2023-11-08 11:08:32 -0500

    Agregar archivos de proyecto.

---
+++ Models/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductoApp.Models
+{
+    class Producto
+    {
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int cantidad { get; set; }
+    }
+}
---
+++ ProductoPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace ProductoApp;
+
+public partial class ProductoPage : ContentPage
+{
+	public ProductoPage()
+	{
+		InitializeComponent();
+	}
+}
